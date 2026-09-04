97e8d9c20f352a36bfdd67a5659ab0ba20e02cf6
richw22jr <richw22@hotmail.com>
2026-09-04 02:11:51 +0000

    completed the todos, compiled and ran program

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
-// Practice 2 — Your Name
+// Practice 2 — Richard Webster
 // CIS 5 Week 02 · Structure polish
 
 int main() {
-  // Block 1 — who you are (from Lab 2)
+
   std::cout << "=== About me ===\n";
-  // TODO: 3+ labeled lines
+  std::cout << "Name: Richard Webster\n";
+  std::cout << "Hometown: Moreno Valley, CA \n";
+  std::cout << "College Major: Electrical Engineering \n";
+            // I chose the major because its difficult
+  std::cout << "Favorite Food: Pizza \n" << "\n";
+            // I love pizza because I love cheese
 
-  // Block 2 — this week's goals (from outcomes)
   std::cout << "=== Week 02 goals ===\n";
-  // TODO: list 3 goals as complete sentences
-
+  std::cout << "1) I became familiar with the 5 basic parts of a C++ program \n"
+            << "   which are #include line, main() line, std::cout console object, \n"
+            << "   insertion operator, and return 0.\n";
+  std::cout << "2) I can write a simple program that prints text organized the \n"
+            << "   way I want it to print. \n";
+  std::cout << "3) I was able to compile with errors a few times \n"
+            << "   but I was able to fix and run the program. \n";
+            // I made a few syntax errors, mainly semicolons, but I corrected it.
+            // I have a little bit of programmin experience so I tried to make it look nice.
   return 0;
 }
